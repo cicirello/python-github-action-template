8fe5460891201b1e7b601f8038c57c419c0f9102
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2021-03-10 15:23:49 -0500

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -8,9 +8,12 @@ import unittest
 # for example running of test cases.
 import entrypoint as ep
 
-class TestTidyjavadocs(unittest.TestCase) :
+class TestSomething(unittest.TestCase) :
 
     def test_sometestcase(self) :
+        # Unit test cases would go in these
+        # test methods to test the various
+        # Python functions, methods, etc.
         pass
 
     def test_anothertestcase(self) :
