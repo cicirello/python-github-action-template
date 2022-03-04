7f74eea8f3c0f6e3c86d7282d08856479d20f5bc
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2022-03-04 15:42:34 -0500

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -2,7 +2,7 @@
 
 # NOTE: If you are using an alpine docker image
 # such as pyaction-lite, the -S option above won't
-# work. The above line works fine on other distributions
+# work. The above line works fine on other linux distributions
 # such as debian, etc, so the above line will work fine
 # if you use pyaction:4.0.0 or higher as your base docker image.
 
